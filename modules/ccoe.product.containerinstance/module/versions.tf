fd82304e955ccf373b58433d6f8d99e95e49e55e
hatan4ik <hatan4ik@gmail.com>
2024-10-11 16:59:24 +0300

    tf UP

--- modules/ccoe.product.containerinstance/module/versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">=3.20.0"
-    }
-  }
-  required_version = ">= 1.3.0"
-}
--- modules/ccoe.product.tools.rbac/module/main.tf
+++
@@ -1,8 +0,0 @@
-
-resource "azurerm_role_assignment" "role_assignment" {
-  count = length(local.schema_role_mappings)
-
-  scope                = var.scope_id
-  role_definition_name = local.schema_role_mappings[count.index].role
-  principal_id         = local.schema_role_mappings[count.index].id
-}
