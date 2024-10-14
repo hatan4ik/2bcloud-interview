f63df21d189a06ff241bef44c0fa393623bbb489
hatan4ik <hatan4ik@gmail.com>
2024-10-14 12:36:10 +0300

    VNet routes fixes and delegations

--- vnet.tf
+++ vnet.tf
@@ -15,26 +15,12 @@ resource "azurerm_subnet" "vm_subnet" {
   service_endpoints    = ["Microsoft.KeyVault", "Microsoft.Storage"]
 }
 
-resource "azurerm_subnet_service_endpoint_storage_policy" "aks_storage_policy" {
-  name                = "aks-storage-endpoint-policy"
-  resource_group_name = data.azurerm_resource_group.main.name
-  location            = data.azurerm_resource_group.main.location
-
-  definition {
-    name        = "AzureStorage"
-    description = "Allow access to Azure Storage"
-    service_resources = [
-      "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/${data.azurerm_resource_group.main.name}/providers/Microsoft.Storage/storageAccounts/*"
-    ]
-  }
-}
-
 resource "azurerm_subnet" "aks_subnet" {
   name                 = "aks-subnet"
   resource_group_name  = data.azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main_vnet.name
   address_prefixes     = ["10.0.2.0/24"]
-  service_endpoints    = ["Microsoft.KeyVault", "Microsoft.Storage"]
+  service_endpoints    = ["Microsoft.KeyVault", "Microsoft.Storage", "Microsoft.ContainerRegistry"]
 
   delegation {
     name = "aksDelegation"
@@ -43,9 +29,6 @@ resource "azurerm_subnet" "aks_subnet" {
       actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
     }
   }
-  service_endpoint_policy_ids = [
-    azurerm_subnet_service_endpoint_storage_policy.aks_storage_policy.id
-  ]
 }
 
 resource "azurerm_subnet" "aks_nodes" {
@@ -64,15 +47,212 @@ resource "azurerm_subnet" "pe_subnet" {
   service_endpoints    = ["Microsoft.KeyVault", "Microsoft.Storage"]
 }
 
+# Network Security Groups
 
-
-# Static Public IP for Ingress
-resource "azurerm_public_ip" "ingress" {
-  name                = "ingress-public-ip"
+# NSG for VM Subnet
+resource "azurerm_network_security_group" "vm_nsg" {
+  name                = "vm-nsg"
   location            = data.azurerm_resource_group.main.location
   resource_group_name = data.azurerm_resource_group.main.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+
+  security_rule {
+    name                       = "AllowInternetOutbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "*"
+    destination_address_prefix = "Internet"
+  }
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "Jenkins"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "8080"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = 1003
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTPS"
+    priority                   = 1004
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "443"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowAKSInbound"
+    priority                   = 1005
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = azurerm_subnet.aks_subnet.address_prefixes[0]
+    destination_address_prefix = "*"
+  }
+}
+
+# NSG for AKS Nodes Subnet
+resource "azurerm_network_security_group" "aks_nodes_nsg" {
+  name                = "aks-nodes-nsg"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "AllowAKSOutbound"
+    priority                   = 100
+    direction                  = "Outbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = azurerm_subnet.aks_nodes.address_prefixes[0]
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "AllowInboundFromVM"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = azurerm_subnet.vm_subnet.address_prefixes[0]
+    destination_address_prefix = "*"
+  }
+}
+
+# NSG for PE Subnet
+resource "azurerm_network_security_group" "pe_nsg" {
+  name                = "pe-nsg"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  security_rule {
+    name                       = "AllowPrivateEndpointInbound"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = "VirtualNetwork"
+    destination_address_prefix = "VirtualNetwork"
+  }
+
+  security_rule {
+    name                       = "AllowAKSInboundToPE"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "*"
+    source_port_range          = "*"
+    destination_port_range     = "*"
+    source_address_prefix      = azurerm_subnet.aks_subnet.address_prefixes[0]
+    destination_address_prefix = "*"
+  }
+}
+
+# Route Tables
+
+# Route Table for VM Subnet
+resource "azurerm_route_table" "vm_route_table" {
+  name                = "vm-route-table"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  route {
+    name           = "InternetRoute"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "Internet"
+  }
+
+  route {
+    name           = "AKSRoute"
+    address_prefix = azurerm_subnet.aks_subnet.address_prefixes[0]
+    next_hop_type  = "VnetLocal"
+  }
+
+  route {
+    name           = "PERoute"
+    address_prefix = azurerm_subnet.pe_subnet.address_prefixes[0]
+    next_hop_type  = "VnetLocal"
+  }
+}
+
+# Route Table for AKS Nodes Subnet
+resource "azurerm_route_table" "aks_route_table" {
+  name                = "aks-route-table"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  route {
+    name           = "VMtoAKSRoute"
+    address_prefix = azurerm_subnet.vm_subnet.address_prefixes[0]
+    next_hop_type  = "VnetLocal"
+  }
+
+  route {
+    name           = "PEtoAKSRoute"
+    address_prefix = azurerm_subnet.pe_subnet.address_prefixes[0]
+    next_hop_type  = "VnetLocal"
+  }
+}
+
+# Route Table for Private Endpoint Subnet
+resource "azurerm_route_table" "pe_route_table" {
+  name                = "pe-route-table"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+
+  route {
+    name           = "VMtoPERoute"
+    address_prefix = azurerm_subnet.vm_subnet.address_prefixes[0]
+    next_hop_type  = "VnetLocal"
+  }
+
+  route {
+    name           = "AKStoPERoute"
+    address_prefix = azurerm_subnet.aks_nodes.address_prefixes[0]
+    next_hop_type  = "VnetLocal"
+  }
 }
 
 # NAT Gateway and Public IP
@@ -103,97 +283,38 @@ resource "azurerm_subnet_nat_gateway_association" "main" {
   nat_gateway_id = azurerm_nat_gateway.main.id
 }
 
-# Network Security Group (NSG)
-resource "azurerm_network_security_group" "vm_nsg" {
-  name                = "vm-nsg"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
+# Associate Route Tables
 
-  security_rule {
-    name                       = "AllowInternetOutbound"
-    priority                   = 100
-    direction                  = "Outbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "*"
-    destination_address_prefix = "Internet"
-    description                = "Allow outbound internet traffic"
-  }
-
-  security_rule {
-    name                       = "SSH"
-    priority                   = 1001
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-    description                = "Allow inbound SSH"
-  }
-
-  security_rule {
-    name                       = "Jenkins"
-    priority                   = 1002
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "8080"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-    description                = "Allow inbound Jenkins traffic"
-  }
-
-  security_rule {
-    name                       = "HTTP"
-    priority                   = 1003
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "80"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-    description                = "Allow inbound HTTP traffic"
-  }
-
-  security_rule {
-    name                       = "HTTPS"
-    priority                   = 1004
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "443"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-    description                = "Allow inbound HTTPS traffic"
-  }
+# Associate Route Table with VM Subnet
+resource "azurerm_subnet_route_table_association" "vm_subnet_route_table_assoc" {
+  subnet_id      = azurerm_subnet.vm_subnet.id
+  route_table_id = azurerm_route_table.vm_route_table.id
 }
 
-resource "azurerm_network_security_group" "pe_nsg" {
-  name                = "pe-nsg"
-  location            = data.azurerm_resource_group.main.location
-  resource_group_name = data.azurerm_resource_group.main.name
-
-  security_rule {
-    name                       = "AllowPrivateEndpointInbound"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "*"
-    source_address_prefix      = "VirtualNetwork"
-    destination_address_prefix = "VirtualNetwork"
-  }
+# Associate Route Table with AKS Nodes Subnet
+resource "azurerm_subnet_route_table_association" "aks_nodes_route_table_assoc" {
+  subnet_id      = azurerm_subnet.aks_nodes.id
+  route_table_id = azurerm_route_table.aks_route_table.id
 }
 
-# Associate NSG with PE subnet
+# Associate Route Table with Private Endpoint Subnet
+resource "azurerm_subnet_route_table_association" "pe_route_table_assoc" {
+  subnet_id      = azurerm_subnet.pe_subnet.id
+  route_table_id = azurerm_route_table.pe_route_table.id
+}
+
+# Associate NSG with Subnets
+
+resource "azurerm_subnet_network_security_group_association" "vm_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.vm_subnet.id
+  network_security_group_id = azurerm_network_security_group.vm_nsg.id
+}
+
+resource "azurerm_subnet_network_security_group_association" "aks_nodes_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.aks_nodes.id
+  network_security_group_id = azurerm_network_security_group.aks_nodes_nsg.id
+}
+
 resource "azurerm_subnet_network_security_group_association" "pe_nsg_assoc" {
   subnet_id                 = azurerm_subnet.pe_subnet.id
   network_security_group_id = azurerm_network_security_group.pe_nsg.id
