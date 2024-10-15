1fad4f162f7c9f61ca7ee29dba0d30fb29697134
hatan4ik <hatan4ik@gmail.com>
2024-10-15 18:42:15 +0300

    Auto comit message

--- milkyway/jenkins-setup.sh
+++ milkyway/jenkins-setup.sh
@@ -21,7 +21,12 @@ sudo apt-get update
  sudo   apt-get install -y openjdk-11-jdk
 
     # Install Jenkins
- sudo  wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | apt-key add -
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+ wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
  sudo  sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
  sudo  apt-get update
  sudo  apt-get install -y jenkins
