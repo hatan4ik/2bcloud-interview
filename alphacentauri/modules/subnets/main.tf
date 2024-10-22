d3bda31b71a7c01e85e53f6bc4ce8df2a7964c22
hatan4ik <hatan4ik@gmail.com>
2024-10-22 21:48:10 +0300

    Auto comit message

--- alphacentauri/modules/subnets/main.tf
+++ alphacentauri/modules/subnets/main.tf
@@ -5,8 +5,23 @@ resource "azurerm_subnet" "subnets" {
   virtual_network_name = var.vnet_name
   address_prefixes     = [each.value.address_prefix]
   service_endpoints    = each.value.service_endpoints
+  network_security_group_id = azurerm_network_security_group.nsgs[each.key].id
+  route_table_id       = azurerm_route_table.custom_routes.id
+
 }
 
 output "subnet_ids" {
   value = { for k, v in azurerm_subnet.subnets : k => v.id }
+}
+
+resource "azurerm_route_table" "custom_routes" {
+  name                = "custom-routes"
+  resource_group_name = var.resource_group_name
+  location            = data.azurerm_resource_group.main.location
+
+  route {
+    name                   = "default-route"
+    address_prefix         = "0.0.0.0/0"
+    next_hop_type          = "Internet"
+  }
 }
