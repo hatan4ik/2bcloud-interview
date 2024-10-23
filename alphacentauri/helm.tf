67f818552e5029b531669a866288eed0081cfb00
hatan4ik <hatan4ik@gmail.com>
2024-10-23 17:52:35 +0300

    Auto comit message

--- alphacentauri/helm.tf
+++ alphacentauri/helm.tf
@@ -1,8 +1,9 @@
 # Install NGINX Ingress Controller
 resource "helm_release" "ingress_nginx" {
-  name             = "ingressnginx"
+  name             =  random_string.random.result
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
+  version          = "4.7.1"
   namespace        = "myapp"
   create_namespace = true
 
@@ -10,18 +11,16 @@ resource "helm_release" "ingress_nginx" {
     name  = "controller.service.loadBalancerIP"
     value = azurerm_public_ip.ingress_public_ip.ip_address
   }
-  # set {
-  #   name  = "controller.service.annotations.kubernetes\\.io/ingress\\.class" # Use correct annotation
-  #   value = "nginx"
-  # }
+  set {
+     name  = "controller.service.annotations.kubernetes\\.io/ingress\\.class" # Use correct annotation
+     value = "nginx"
+   }
     set {
      name  = "controller.service.annotations.service\\.beta\\.kubernetes\\.io/azure-load-balancer-resource-group"
      value = data.azurerm_resource_group.main.name
    }
 
-  depends_on = [azurerm_kubernetes_cluster.aks,
-  azurerm_public_ip.ingress_public_ip,
-  azurerm_role_assignment.aks_network_contributor,
-  azurerm_role_assignment.aks_node_rg_network_contributor
-  ]
+  # depends_on = [azurerm_kubernetes_cluster.aks,
+  # azurerm_public_ip.ingress_public_ip
+  #]
 }
